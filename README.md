e2fff9bb82ee7d041015e78b9cd1e468820ad804
Yao Zhao <yao.zhao.ai@gmail.com>
2021-05-06 16:36:38 +0000

    update to retrieve all docs

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # retrievalgen
 retrieval generation pipeline modified from https://yjernite.github.io/lfqa.html
+
+
+```
+wget https://us.download.nvidia.com/tesla/460.73.01/NVIDIA-Linux-x86_64-460.73.01.run
+```
+wget https://developer.download.nvidia.com/compute/cuda/11.3.0/local_installers/cuda_11.3.0_465.19.01_linux.run
--- retrieve_wiki.py
+++ retrieve_wiki.py
@@ -1,3 +1,6 @@
+import json
+from tqdm import tqdm
+
 import faiss
 import numpy as np
 import pandas as pd
@@ -5,7 +8,7 @@ import pandas as pd
 import lfqa_utils
 import utils
 
-# faiss_res = faiss.StandardGpuResources()
+
 wiki40b_passage_reps = np.memmap(
     utils.SAVED_REPRESENTATIONS,
     dtype='float32', mode='r',
@@ -13,29 +16,28 @@ wiki40b_passage_reps = np.memmap(
 )
 
 wiki40b_index_flat = faiss.IndexFlatIP(128)
-wiki40b_gpu_index = faiss.index_cpu_to_all_gpus(  # build the index
-    wiki40b_index_flat
-)
-# wiki40b_gpu_index = faiss.index_cpu_to_gpu_multiple(faiss_res, [1], wiki40b_index_flat)
+wiki40b_gpu_index = faiss.index_cpu_to_all_gpus(wiki40b_index_flat)
 wiki40b_gpu_index.add(wiki40b_passage_reps)
 
 qar_args = utils.ArgumentsQAR()
 qar_tokenizer, qar_model = lfqa_utils.make_qa_retriever_model(
     model_name=qar_args.pretrained_model_name,
     from_file=f"{utils.SAVED_RETRIEVER}_9.pth",
-    device="cuda:1"
+    device="cuda:0"
 )
 _ = qar_model.eval()
 
-question = utils.ELI5['test_eli5'][12345]['title']
-doc, res_list = lfqa_utils.query_qa_dense_index(question, qar_model, qar_tokenizer, utils.WIKI40B_SNIPPETS,
-                                                wiki40b_gpu_index, device='cuda:1')
+batch_size = 32
 
-df = pd.DataFrame({
-    'Article': ['---'] + [res['article_title'] for res in res_list],
-    'Sections': ['---'] + [res['section_title'] if res['section_title'].strip() != '' else res['article_title']
-                           for res in res_list],
-    'Text': ['--- ' + question] + [res['passage_text'] for res in res_list],
-})
-df.style.set_properties(**{'text-align': 'left'})
-print(df)
+for split in ["validation_eli5", "test_eli5", "train_eli5"]:
+  print(f"processing {split}")
+  with open(f"output/{split}.jsonl", "w") as f:
+    datasets = utils.ELI5[split]
+    for i in tqdm(range(0, len(datasets), batch_size)):
+      rows = [datasets[j] for j in range(i, min(i+batch_size, len(datasets)))]
+      questions = [r["title"] for r in rows]
+      _, all_res_lists = lfqa_utils.batch_query_qa_dense_index(questions, qar_model, qar_tokenizer, utils.WIKI40B_SNIPPETS,
+                                                wiki40b_gpu_index)
+      for row, res_list in zip(rows, all_res_lists):
+        row["doc"] = res_list
+        f.write(json.dumps(row) + "\n")
